767ace2cb43a072f14e76228eaf063c99a990d1e
Jarvis Silva <77526704+Jarvissilva@users.noreply.github.com>
2023-01-14 04:25:14 +0000

    fibonacci program

--- index.py
+++ index.py
@@ -1,1 +1,19 @@
-print("Hello world")
+num = int(input("Enter a number: "))
+a = 0
+b = 1
+nums = [0,1]
+isFound = False
+
+for i in range(num):
+    nums.append(a+b)
+    ind = nums.index(a+b,2)
+    if(nums[ind] == num):
+        print(f"{num} is fibonacci")
+        isFound = True
+        break
+    else:
+        a = nums[ind]
+        b = nums[ind - 1]
+
+if(isFound == False):
+    print("Not fibonacci")
